fb63ad41621827b723a827d02003c946068e04fe
jkc1996 <jaykumarchaudhary.nmims@gmail.com>
2019-10-20 22:45:09 +0530

    3 files modified

--- home/home.controller.js
+++ home/home.controller.js
@@ -3,7 +3,16 @@ class HomeCtrl {
   constructor($scope, UserService, $location) {
     'ngInject';
 
-    this.name = "AngularJS";
+    this.name = '';
+    this.gender= '';
+    this.phNumber='';
+    this.trainingMode='';
+    this.courseSelected='';
+    this.courses=[];
+    this.levels=['Level 1', 'Level 2', 'Level 3'];
+    this.level='';
+    this.levelType='';
+    this.message='';
     this.$Uservice = UserService;
     this.users = [];
     this.$scope = $scope;
@@ -14,6 +23,42 @@ class HomeCtrl {
     this.$scope.$applyAsync();
   }
 
+  getCourses() {
+      if (this.trainingMode == 'VCR') {
+        this.courses = [{coursename: 'HTML'},{coursename: 'CSS'},{coursename: 'JS'}]
+      }
+      else {
+         this.courses = [{coursename: 'AngularJs'},{coursename: 'BackboneJs'}]
+      }
+    }
+
+  selectedLevel () {
+    if (this.level == 'Level 1'){
+      this.levelType = 'this is advance level'
+    }
+    else if (this.level == 'Level 2') {
+      this.levelType = 'this is medium level'
+    }
+    else {
+      this.levelType = 'this is basic level'
+
+    }
+  }
+
+  save () {
+    if (this.gender == 'male') {
+      this.message = "Mr." + this.name + " has opted for " + this.level + " level training on " + this.courseSelected + " in " + this.trainingMode + " mode.";
+            this.message += "He can be contacted at:" + this.phNumber;
+    }
+    else {
+       this.message = "Ms." + this.name + " has opted for " + this.level + " level training on " + this.courseSelected + " in " + this.trainingMode + " mode.";
+    }
+  }
+buttonDisabeled () {
+  if (this.name == '' || this.gender == '' || this.courseSelected =='' || this.trainingMode =='' || this.level == '') {
+    return true;
+  }
+}
 }
 
 export default HomeCtrl;
